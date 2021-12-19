fee6f9b6985ccf5549dd1afc9e1be43cec6d0c0d
Vishalk786 <46914039+Vishalk786@users.noreply.github.com>
2021-12-19 00:56:56 -0500

    Add files via upload

---
+++ While_loop.java
@@ -0,0 +1,13 @@
+class Display
+{ 
+ public static void main(String arg[])
+ { 
+  int value=1;
+  while (value<=10)
+  { 
+   System.out.println(value);
+   value++;
+   }
+   System.out.println("End of the Loop");
+  } 
+}
