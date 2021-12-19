3233e1f5e49d1587c03f3010c749569a02c0ad5b
Vishalk786 <46914039+Vishalk786@users.noreply.github.com>
2021-12-19 00:57:31 -0500

    Add files via upload

---
+++ Break_Statement.java
@@ -0,0 +1,16 @@
+class DemoBreak
+{
+ public static void main(String arg[])
+ { 
+  int num;
+  for(num=1;num<=10;num++)
+  { 
+   if (num==5)
+  {
+  System.out.println("Break encounted");
+  }
+  System.out.println(num);
+  }
+  System.out.println("End of the Program.");
+  }
+}
